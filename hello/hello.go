ca23d1f09017d8b843addac32fc4baca50f252d1
Yasin Kinyua <yasinkinyua@gmail.com>
2021-12-06 21:19:13 +0300

    add more tests

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,9 @@ package hello
 const EnglishHelloPrefix = "Hello"
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+
 	return EnglishHelloPrefix + ", " + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Yaska")
-	want := "Hello, Yaska"
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Yaska")
+		want := "Hello, Yaska"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("use 'World' when string is empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
